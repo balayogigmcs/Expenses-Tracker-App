69a333cb67a4f5f062a7357c936ff1b729e959ab
balayogi789 <balyogigmcs@gmail.com>
2024-04-19 15:53:36 -0400

    dummy data addition

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expenses_tracker_app/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,11 +11,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'movie',
+        amount: 15,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+
   @override
   Widget build(context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-      body: Column(),
+      body: Column(
+        
+      ),
     );
   }
 }
