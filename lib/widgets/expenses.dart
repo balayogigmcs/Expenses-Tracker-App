03aa00d29e4b5453b387678399a19cd317b1689a
balayogi789 <balyogigmcs@gmail.com>
2024-04-19 19:25:38 -0400

    appbar and showModalBottomSheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:expenses_tracker_app/models/expense.dart';
 import 'package:expenses_tracker_app/widgets/expenses_list/expenses_list.dart';
 
-
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -27,9 +26,19 @@ class _ExpensesState extends State<Expenses> {
   ];
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Expenses Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add,size: 50,),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text(' The Chart'),
